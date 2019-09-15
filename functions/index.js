69180c9431c69243178898d264201d8b5472e71b
omegalink12 <omegalink12@users.noreply.github.com>
2019-09-14 23:38:30 -0400

    Add getHash

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,20 @@
 const functions = require('firebase-functions');
 
+const algosdk = require('algosdk');
+
+//Retrieve the token, server and port values for your installation in the algod.net
+//and algod.token files within the data directory
+const atoken = {'X-API-Key':"SYnCl5Mzrt3H2k6OoN9ca5IJcQ7pe9MMAk3t7uPh"};
+const aserver = "https://testnet-algorand.api.purestake.io/ps1";
+
+const algodclient = new algosdk.Algod(atoken, aserver, "");
+
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.getHash = functions.https.onRequest(async (request, response) => {
+	let lastround = (await algodclient.status()).lastRound;
+	let block = (await algodclient.block(lastround));
+	response.send(block)
+});
